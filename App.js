b58083563a6e2227d63f9ddb43411127e84180b9
Shay Mautner <shay.mautner@ruppin365.net>
2022-02-10 09:07:19 +0200

    installation of client side

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import searchParking from './Pages/searchParking';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="SearchParking">
+      <Stack.Screen name="SearchParking" component={searchParking} options={{ title: 'Search Parking' }}/>
+      </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
